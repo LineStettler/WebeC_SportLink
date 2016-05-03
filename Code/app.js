1f0e1c2a296dbb0fe4c1c2a745f7d8048f19c813
Line Stettler <line.stettler@students.fhnw.ch>
2016-05-03 22:33:26 +0200

    Mock Version1 and first html, css and js switching between the section is working pulldown menu is working

---
+++ Code/app.js
@@ -0,0 +1,60 @@
+/**
+ * Created by Line on 03/05/16.
+ */
+
+var LOGIN = '#Login';
+var REGISTER = '#Register';
+var STARTSEITE = '#Startseite';
+var OFFENEANFRAGEN = '#Offene_Anfragen';
+var MEINEANFRAGEN = '#Meine_Anfragen';
+var ANFRAGEERSTELLEN = '#Anfrage_Erstellen';
+var TERMINE = '#Termine';
+
+function showSection(id){
+    $('section').hide();
+    $(id).show();
+}
+
+$(document).ready(function() {
+    showSection(LOGIN);
+    //$('#navigation').hide();
+
+    $('#buger').click( function(){
+        $('#navigation').show();
+    });
+
+    $('#Startseite-Link').click( function(){
+        showSection(STARTSEITE);
+    });
+    $('#Offene_Anfragen-Link').click( function(){
+        showSection(OFFENEANFRAGEN);
+    });
+    $('#Meine_Anfragen-Link').click( function(){
+        showSection(MEINEANFRAGEN);
+    });
+    $('#Anfrage_Erstellen-Link').click( function(){
+        showSection(ANFRAGEERSTELLEN);
+    });
+    $('#Termine-Link').click( function(){
+        showSection(TERMINE);
+    });
+    $('#Logout-Link').click( function(){
+        showSection(LOGIN);
+    });
+    $('#LoginButton').click( function(){
+        showSection(STARTSEITE);
+    });
+    $('#RegisterLink').click( function(){
+        showSection(REGISTER);
+    });
+    $('#OffeneAnfragenButton').click( function(){
+        showSection(OFFENEANFRAGEN);
+    });
+    $('#AnfrageErstellenButton').click( function(){
+        showSection(ANFRAGEERSTELLEN);
+    });
+    $('#AnfrageErstelltButton').click( function(){
+        showSection(MEINEANFRAGEN);
+    });
+
+});
