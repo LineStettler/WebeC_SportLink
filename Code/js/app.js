600c735568e53791bc442d13e1b1e499bfa0fd23
Line Stettler <line.stettler@students.fhnw.ch>
2016-05-24 19:17:23 +0200

    new styling with materialize()

--- Code/js/app.js
+++ Code/js/app.js
@@ -60,4 +60,25 @@ $(document).ready(function() {
         showSection(MEINEANFRAGEN);
     });
 
+    $('.button-collapse').sideNav({
+            menuWidth: 300, // Default is 240
+            edge: 'left', // Choose the horizontal origin
+            closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        }
+    );
+
+    $(document).ready(function() {
+        Materialize.updateTextFields();
+    });
+
+    $(document).ready(function() {
+        $('select').material_select();
+    });
+
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
+
+
 });
